b80e479e6a8d539322f774d739de47c81b7950b3
Narcisa-Ionela Guran <narcisaguran97@gmail.com>
2024-01-18 14:55:34 +0200

    Switched to right adjusted reading from ADC

--- main.cpp
+++ main.cpp
@@ -2,73 +2,50 @@
 #include <util/delay.h>
 #include "config.h"
 
-#define ADMUX_DEFAULT 0b01100000
+#define ADMUX_DEFAULT 0b01000000
 
-#define SPI_DDR DDRB
-#define CS      PINB2
-#define MOSI    PINB3
-#define MISO    PINB4
-#define SCK     PINB5
-
-void SPI_init()
-{
-	// set CS, MOSI and SCK to output
-	SPI_DDR |= (1 << CS) | (1 << MOSI) | (1 << SCK);
-
-	// enable SPI, set as master, and clock to fosc/128
-	SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR1) | (1 << SPR0);
-}
-
-void SPI_masterTransmitByte(uint8_t data)
-{
-	// load data into register
-	SPDR = data;
-
-	// Wait for transmission complete
-	while(!(SPSR & (1 << SPIF)));
-}
-
-int fvoltage3(void){//ADC3
+int fvoltage3(void){//ADC3, C4
 	ADMUX = ADMUX_DEFAULT | 3;
 	ADCSRA |= (1<<ADSC);
 	while((ADCSRA& (1<<ADIF)) ==0); //wait until conversion is done
 	ADCSRA |= (1<<ADIF);
 	
-	uint16_t voltage3 = ((ADCL << 8) | ADCH);
+	uint16_t voltage3 = ADC;
 	
 	return voltage3;
 }
 
-int fvoltage2(void){//ADC2
+int fvoltage2(void){//ADC2, C3
 	ADMUX = ADMUX_DEFAULT | 2;
 	ADCSRA |= (1<<ADSC);
 	while((ADCSRA& (1<<ADIF)) ==0); //wait until conversion is done
 	ADCSRA |= (1<<ADIF);
 	
-	uint16_t voltage2 = ((ADCL << 8) | ADCH);
-	
+	uint16_t voltage2 = ADC ;
+
 	return voltage2;
 }
 
-int fvoltage1(void){//ADC1
+int fvoltage1(void){//ADC1, C2
 	ADMUX = ADMUX_DEFAULT | 1;
 	ADCSRA |= (1<<ADSC);
 	while((ADCSRA& (1<<ADIF)) ==0); //wait until conversion is done
 	ADCSRA |= (1<<ADIF);
 	
-	uint16_t voltage1 = ((ADCL << 8) | ADCH);
+	uint16_t voltage1 = ADC;
 	
 	return voltage1;
 }
 
-int fvoltage0(void){//ADC0
+
+int fvoltage0(void){//ADC0, C1
 	ADMUX = ADMUX_DEFAULT;
 	ADCSRA |= (1<<ADSC);
+	
 	while((ADCSRA& (1<<ADIF)) ==0); //wait until conversion is done
 	ADCSRA |= (1<<ADIF);
 	
-	uint16_t voltage0 = ((ADCL << 8) | ADCH);
-	
+	uint16_t voltage0 = ADC;
 	return voltage0;
 }
 
@@ -90,20 +67,19 @@ int main(void){
 		voltage[3] = fvoltage3();
 		
 		minVal = voltage[0];
-		for(int i = 1; i < 4; i++){
+		for(int i = 0; i < 4; ++i){
 			if(voltage[i] < minVal) minVal = voltage[i];
 		}
 		
-		if(voltage[0] >  (minVal + thVal)) PORTD = 0x08; //PD3 HIGH
+		if(voltage[0] > (minVal + thVal)) PORTD = 0x08; //PD3 HIGH
 		if(voltage[1] >  (minVal + thVal)) PORTD |= 0x20; //PD5 HIGH
 		if(voltage[2] >  (minVal + thVal)) PORTD |= 0x40; //PD6 HIGH
 		if(voltage[3] >  (minVal + thVal)) PORTB = 0x02; //PB1 HIGH
 				
-		_delay_ms(10000);
+		_delay_ms(4000);
 		PORTB = 0x00;
 		PORTD = 0x00;
-		_delay_ms(10000);
-		
+		_delay_ms(4000);
 	}
 	return 0;
 }
